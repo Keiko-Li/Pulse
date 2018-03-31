6c7996093f0ae59d7767ef6a24042b5cdb005d0d
laceyLi <1741211509@qq.com>
2018-03-31 21:25:19 +0800

    Add files via upload

---
+++ algorithm.h
@@ -0,0 +1,14 @@
+/*
+ * algorithm.h
+ *
+ *  Created on: 2018年3月11日
+ *      Author: laceyli
+ */
+
+#ifndef INC_TASKS_ALGORITHM_H_
+#define INC_TASKS_ALGORITHM_H_
+
+extern unsigned char Pulse_algorithm(unsigned char *data);
+extern unsigned char Pulse_Measure(void);
+
+#endif /* INC_TASKS_ALGORITHM_H_ */
---
+++ button.h
@@ -0,0 +1,17 @@
+/*
+ * button.h
+ *
+ *  Created on: 2018年3月15日
+ *      Author: laceyli
+ */
+
+#ifndef INC_TASKS_BUTTON_H_
+#define INC_TASKS_BUTTON_H_
+
+extern void GPIO_init();
+extern void P1_IODect();
+extern void P2_IODect();
+extern void P11_Onclick();
+extern void P21_Onclick();
+
+#endif /* INC_TASKS_BUTTON_H_ */
